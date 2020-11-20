f9359fdddfe89d459367b2b678d5b3a8395374f6
Azukyoz <guangchongli@gmail.com>
2020-11-20 16:13:30 -0500

    add converter

--- GL/js/script.js
+++ GL/js/script.js
@@ -18,3 +18,32 @@ $(function () { // Same as document.addEventListener("DOMContentLoaded"...
 function myFunction() {
     document.getElementById("showCode").classList.toggle("show");
 }
+
+
+
+function lbToKg(valNum) {
+  document.getElementById("kgs").value = valNum/2.205;
+}
+
+function kgToLb(valNum) {
+  document.getElementById("lbs").value = valNum*2.205;
+}
+
+function cToF(valNum) {
+  document.getElementById("fahrenheit").value = valNum*9/5+32;
+}
+
+function fToC(valNum) {
+  document.getElementById("celsius").value = (valNum-32)*5/9;
+}
+
+function mileToKilometer(valNum) {
+  document.getElementById("kilometer").value = valNum*1.609;
+}
+
+function kilometerToMile(valNum) {
+  document.getElementById("mile").value = valNum/1.609;
+}
+
+
+
